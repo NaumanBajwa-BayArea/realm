5b586637392860270383e5345b6126fc0da3741d
NaumanBajwa-BayArea <drnaumanbajwadds@gmail.com>
2019-12-01 20:33:20 -0800

    Initial Commit

---
+++ realm/controller/CategoryTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  realm
+//
+//  Created by Nauman Bajwa on 12/1/19.
+//  Copyright © 2019 Nauman Bajwa. All rights reserved.
+//
+
+import UIKit
+
+class CategoryTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
